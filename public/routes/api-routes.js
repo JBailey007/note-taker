bf8402790be1fdbef7d764885c268cd631eb5098
Jalen <jwbailey13@gmail.com>
2023-09-17 21:28:07 -0600

    added the /delete route for the api-routes file

--- public/routes/api-routes.js
+++ public/routes/api-routes.js
@@ -4,13 +4,13 @@ const { v4: uuidv4 } = require('uuid');
 
 //Defines where the get route will point
 router.get('/api/notes', async (req, res) => {
-    const dataJson = await JSON.parse(fs.readFileSync("db/db.json", "utf-8"));
+    const dataJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     res.json(dataJson);
 });
 
 //Defines where the post route will point
 router.post('/api/notes', (req, res) => {
-    const dataJson = JSON.parse(fs.readFileSync("db/db.json", "utf-8"));
+    const dataJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     const newFeedback = {
         title: req.body.title,
         text: req.body.text,
@@ -20,3 +20,15 @@ router.post('/api/notes', (req, res) => {
     fs.writeFileSync("db/db.json",JSON.stringify(dataJson));
     res.json(dataJson);
 });
+//Defines what the delete route will do
+router.delete('/api/notes/:id', (req, res) => {
+    let data = fs.readFileSync("db/db.json", "utf8");
+    const dataJSON = JSON.parse(data);
+    const newNotes = dataJSON.filter((note) => {
+        return note.id !== req.params.id;
+    });
+    fs.writeFileSync("db/db.json", JSON.stringify(newNotes))
+    res.json("Note deleted.");
+});
+
+module.exports = router;
